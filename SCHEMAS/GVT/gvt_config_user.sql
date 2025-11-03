d89595531a68cf2fcdfd3bfe3f6dd96b523ae533
armindo.binje <goncalvesbinje@hotmail.com>
2025-11-03 12:00:18 +0100

    Adição estrutua de tabelas

---
+++ SCHEMAS/GVT/gvt_config_user.sql
@@ -0,0 +1,19 @@
+create user kene_conde identified by 123456;
+create user osvaldo_avelino identified by 123456;
+create user armindo_binje identified by 123456;
+create user obede_luanda identified by 123456;
+create user fiau_oliveira identified by 123456;
+create user mamenji_samuel identified by 123456;
+
+grant create session to kene_conde;
+grant create session to osvaldo_avelino;
+grant create session to armindo_binje;
+grant create session to obede_luanda;
+grant create session to fiau_oliveira;
+grant create session to mamenji_samuel;
+
+GRANT dba TO kene_conde;
+GRANT dba TO osvaldo_avelino;
+GRANT dba TO armindo_binje;
+
+/
